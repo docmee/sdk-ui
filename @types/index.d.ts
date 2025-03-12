7b8d4a27ae9eb639870a3b28e5b48b2c23ff2d3f
Micahfang <micah.fang@outlook.com>
2025-03-12 17:00:42 +0800

    ✨ feat(index): add creatorVersion and css options
    - 【新增】增加creatorVersion字段，支持v1和v2两个版本
    - 【新增】增加css字段，支持自定义css样式
    - 【修改】修改example中token的获取地址
    - 【修复】修复类型定义文件

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,14 +1,15 @@
 declare global {
   type EntryPage =
-    | 'dashboard'
-    | 'editor'
-    | 'creator'
-    | 'creator-v2'
-    | 'customTemplate'
-    | 'templateCreator'
-    | 'templateMarker'
+    | "dashboard"
+    | "editor"
+    | "creator"
+    | "creator-v2"
+    | "customTemplate"
+    | "templateCreator"
+    | "templateMarker";
 
   interface InitOptions {
+
     /**
      * 容器
      *  如果传入字符串，则表示这个容器元素的id
@@ -16,55 +17,28 @@ declare global {
      * @example 'id' | HTMLDivElement
      * @import
      */
-    container: HTMLDivElement | string
+    container: HTMLDivElement | string;
     /** token*/
-    token: string
+    token: string;
 
     /** 进入的页面 */
-    page: EntryPage
+    page: EntryPage;
 
     /** 进入editor页面时必须传递pptId参数 */
-    pptId?: string
+    pptId?: string;
 
     /**
      * 所有事件的回调都会执行
      */
-    onMessage?: EventCallback
+    onMessage?: EventCallback;
 
     /**
      * 进入editor页面时，是否显示制作动画
      */
-    animation?: boolean
+    animation?: boolean;
 
-    mode?: 'light' | 'dark'
-    lang?: string
-
-    creatorData?: {
-      files?: File[]
-      content?: string
-      subject?: string
-      type: CreatorType
-      options?: CreatorDataOptions
-    }
-  }
-
-  interface CreatorDataOptions {
-    /** 篇幅长度：short/medium/long => 10-15页/20-30页/25-35页 */
-    length?: string
-    /** 演示场景：通用场景、教学课件、工作总结、工作计划、项目汇报、解决方案、研究报告、会议材料、产品介绍、公司介绍、商业计划书、科普宣传、公众演讲 等任意场景类型。 */
-    scene?: string
-    /** 受众：大众、学生、老师、上级领导、下属、面试官、同事 等任意受众类型。 */
-    audience?: string
-    /** 是否开启反问模式（对话模式） */
-    questionMode?: boolean
-    /** 是否开启智能搜索 */
-    aiSearch?: boolean
-    /** 语言: zh/zh-Hant/en/ja/ko/ar/de/fr/it/pt/es/ru */
-    lang?: string
-    /** 用户要求（小于50字） */
-    prompt?: string
-    /** **多轮对话**  助手反问聊天对话，反问过程中如需终止直接生成请设置 questionMode 为 false 调用接口*/
-    messages?: GenerateContentContextMessage[]
+    mode?: "light" | "dark";
+    lang?: string;
 
     /** 创建PPT 版本, V1是步骤式，V2是对话式 */
     creatorVersion?: 'v1' | 'v2'
@@ -77,11 +51,40 @@ declare global {
      * @example https://abc.cn/style.css
      * */
     css?: string
+
+
+    creatorData?: {
+      files?: File[];
+      content?: string;
+      subject?: string;
+      type: CreatorType;
+      options?: CreatorDataOptions;
+    };
+  }
+
+  interface CreatorDataOptions {
+    /** 篇幅长度：short/medium/long => 10-15页/20-30页/25-35页 */
+    length?: string;
+    /** 演示场景：通用场景、教学课件、工作总结、工作计划、项目汇报、解决方案、研究报告、会议材料、产品介绍、公司介绍、商业计划书、科普宣传、公众演讲 等任意场景类型。 */
+    scene?: string;
+    /** 受众：大众、学生、老师、上级领导、下属、面试官、同事 等任意受众类型。 */
+    audience?: string;
+    /** 是否开启反问模式（对话模式） */
+    questionMode?: boolean;
+    /** 是否开启智能搜索 */
+    aiSearch?: boolean;
+    /** 语言: zh/zh-Hant/en/ja/ko/ar/de/fr/it/pt/es/ru */
+    lang?: string;
+    /** 用户要求（小于50字） */
+    prompt?: string;
+    /** **多轮对话**  助手反问聊天对话，反问过程中如需终止直接生成请设置 questionMode 为 false 调用接口*/
+    messages?: GenerateContentContextMessage[];
+
   }
 
   interface GenerateContentContextMessage {
-    role: 'assistant' | 'user'
-    content: string
+    role: "assistant" | "user";
+    content: string;
   }
 }
 
